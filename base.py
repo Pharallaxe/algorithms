7d15f24d60eba6cb2b070380d172632262314d7e
pharallaxe <bgnewsletter@pm.me>
2024-03-21 21:02:18 +0100

    Initiation du Git et ajout du fichier base.py.

---
+++ base.py
@@ -0,0 +1,45 @@
+# -----------------------------------------------
+# Nom de la fonction
+# -----------------------------------------------
+# Définition.
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+# Version Courte
+# -----------------------------------------------
+# Cette version permet de comprendre le contexte
+# de la fonction et sa logique.
+
+
+
+# -----------------------------------------------
+# Application
+# -----------------------------------------------
+
+
+# -----------------------------------------------
+# Version Golf
+# -----------------------------------------------
+# Version condensée et optimisée du code, utili-
+# sant des noms de variables courts et combinant
+# certaines opérations pour réduire la taille du
+# code. Pour la beauté du geste !
+
+
+
+# -----------------------------------------------
+# Version Détaillée
+# -----------------------------------------------
+# Cette version permet de suivre pas à pas l'exé-
+# cution de la fonction.
+
+
+
+# -----------------------------------------------
+# Version Commentée
+# -----------------------------------------------
+# Similaire à la version détaillée, mais avec des
+# commentaires concis pour expliquer les étapes
+# principales de la fonction.
